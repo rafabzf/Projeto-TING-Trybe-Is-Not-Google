d8a94cc47641e11a08aa0a4ad62e43ed9ae1905e
Rafael <rafinhabzf@gmail.com>
2023-06-16 22:26:33 -0300

    Correção do requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,13 +2,13 @@ import sys
 
 
 def txt_importer(path_file):
-    final = path_file.endswitch(".txt")
+    final = path_file.endswith(".txt")
     if not final:
-        print('Formato inválido', file=sys.stderr)
+        print("Formato inválido", file=sys.stderr)
         return None
     try:
         with open(path_file) as file:
-            return file.read().split('/n')
-    except FileExistsError:
-        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+            return file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
         return None
