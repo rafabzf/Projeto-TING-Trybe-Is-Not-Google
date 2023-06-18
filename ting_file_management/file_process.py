2d0afc8d9903299dcc85c313d3bb65943872f06a
Rafael <rafinhabzf@gmail.com>
2023-06-18 19:51:17 -0300

    Req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,27 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    duplicate = False
+
+    for i in range(instance.__len__()):
+        dict_find = instance.search(i)
+
+        if path_file == dict_find["nome_do_arquivo"]:
+            duplicate = True
+
+    if not duplicate:
+        file = txt_importer(path_file)
+
+        dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file,
+        }
+
+        instance.enqueue(dict)
+        sys.stdout.write(f"{dict}\n")
 
 
 def remove(instance):
