9692caf6f90b2b57fa6d7bf9e1b552494d0e9165
Rafael <rafinhabzf@gmail.com>
2023-06-19 14:51:13 -0300

    Req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files = []
+
+    for file in range(len(instance)):
+        info = instance.search(file)
+        name = info["nome_do_arquivo"]
+        lines = info["linhas_do_arquivo"]
+        res = []
+        for i, l in enumerate(lines):
+            if word.lower() in l.lower():
+                res.append({
+                    "linha": i + 1
+                })
+        if res:
+            result = {
+                "palavra": word,
+                "arquivo": name,
+                "ocorrencias": res
+            }
+            files.append(result)
+    return files
 
 
 def search_by_word(word, instance):
